f6430e3178ae477a9696de05221817254a2f1906
SaharshShu <ss1018656@student.francistuttle.edu>
2021-02-02 20:38:49 -0600

    Establised Connection

--- ifconnect.py
+++ ifconnect.py
@@ -1,11 +1,19 @@
 import socket
-UDP_IP = "127.0.0.1"
-UDP_PORT = 15000
 
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
-sock.bind((UDP_IP, UDP_PORT))
 
+udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+udp.bind(("", 15000))
 while True:
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print("received message: %s" % data)
+    data, addr = udp.recvfrom(4096)
+    if data:
+        print(data)
+        break
+udp.close()
+
+UDP_IP = addr[0]
+UDP_PORT = 10112
+ADDR = (UDP_IP, UDP_PORT)
+
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcp.connect(ADDR)
+print(f"Connected to {UDP_IP} on {UDP_PORT}")
