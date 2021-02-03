ad59600a2f57d3eb83df3978641019c543abef54
SaharshShu <ss1018656@student.francistuttle.edu>
2021-02-02 20:27:42 -0600

    Establish UDP Connection with Infinite Flight

--- ifconnect.py
+++ ifconnect.py
@@ -1,12 +1,11 @@
 import socket
-import json
+UDP_IP = "127.0.0.1"
+UDP_PORT = 15000
 
-localport = 10112
-localip = 127.0.0.1
-
-ifudp = socket.socket(socket.AF_IFNET, socket.SOCK_DGRAM)
-ifupd.bind(("", 15000))
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+sock.bind((UDP_IP, UDP_PORT))
 
 while True:
-    data, addr = ifudp.recvfrom(4096)
-    print("recieved")
+    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    print("received message: %s" % data)
